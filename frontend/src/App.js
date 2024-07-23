dfb5fe810288e0661ef0a0418e4a904f27ec26b8
Agus <agustinabmendez@gmail.com>
2024-07-22 21:31:17 -0300

    creating responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
 import Layout from "./components/layout/layout";
 import "./styles/globals.css";
-import "./styles/App.css";
 
 function App() {
   return (
     <Layout>
       <div className="App">
         <Router basename={process.env.PUBLIC_URL}>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-            </nav>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
         </Router>
       </div>
     </Layout>
